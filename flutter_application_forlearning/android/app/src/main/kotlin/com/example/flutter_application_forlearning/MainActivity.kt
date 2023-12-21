c348233d9072dd64c48b30b8abeee2fb688cd55d
Elif Hüsna Turkay <elifhusnaturkay@hotmail.com>
2023-12-21 22:33:09 +0300

    Initial commit

---
+++ flutter_application_forlearning/android/app/src/main/kotlin/com/example/flutter_application_forlearning/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.flutter_application_forlearning
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ flutter_application_forlearning/lib/main.dart
@@ -0,0 +1,103 @@
+import 'dart:ui';
+
+import 'package:flutter/material.dart';
+
+void main(List<String> args) {
+  runApp(MyFirstApp());
+}
+
+class MyFirstApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+        debugShowCheckedModeBanner: false,
+        theme: ThemeData(
+            primaryColorLight: Color.fromARGB(255, 81, 39, 32),
+            backgroundColor: Color.fromARGB(255, 255, 254, 250)),
+        home: Scaffold(
+          appBar: AppBar(
+              backgroundColor: Color.fromARGB(255, 220, 216, 210),
+              title: Text('My First App'),
+              titleTextStyle: const TextStyle(
+                  fontSize: 40, color: Color.fromARGB(255, 131, 36, 67))),
+          body: Container(
+            color: Color.fromARGB(255, 173, 178, 192),
+            // ignore: sort_child_properties_last
+            child: Padding(
+              padding: EdgeInsets.all(12),
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                children: <Widget>[
+                  Container(
+                    decoration: BoxDecoration(
+                      borderRadius: BorderRadius.circular(10),
+                      border: Border.all(color: Colors.transparent),
+                      color: Color.fromARGB(255, 255, 251, 245),
+                    ),
+                    padding: EdgeInsets.all(12),
+                    height: 200,
+                    width: 400,
+                    child: Padding(
+                      padding: const EdgeInsets.all(10),
+                      child: Container(
+                        decoration: BoxDecoration(
+                          borderRadius: BorderRadius.circular(10),
+                          border: Border.all(color: Colors.transparent),
+                          color: Color.fromARGB(255, 255, 251, 245),
+                        ),
+                        child: Row(
+                          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                          children: [
+                            SizedBox(
+                              height: 200,
+                              width: 200,
+                              child: Image.network(
+                                  "https://avatars.githubusercontent.com/u/126392200?v=4"),
+                            ),
+                            Text("mustafam")
+                          ],
+                        ),
+                      ),
+                    ),
+                  ),
+                  Container(
+                    decoration: BoxDecoration(
+                      borderRadius: BorderRadius.circular(10),
+                      border: Border.all(color: Colors.transparent),
+                      color: Color.fromARGB(255, 255, 251, 245),
+                    ),
+                    height: 200,
+                    width: 400,
+                    child: Row(
+                      children: [],
+                    ),
+                  ),
+                  Container(
+                    decoration: BoxDecoration(
+                      borderRadius: BorderRadius.circular(10),
+                      border: Border.all(color: Colors.transparent),
+                      color: Color.fromARGB(255, 255, 251, 245),
+                    ),
+                    height: 200,
+                    width: 400,
+                    child: Row(
+                      children: [],
+                    ),
+                  ),
+                ],
+              ),
+            ),
+          ),
+          floatingActionButton: FloatingActionButton(
+            shape: CircleBorder(),
+            backgroundColor: Color.fromARGB(255, 66, 94, 117),
+            onPressed: () {
+              debugPrint('Tıklandı');
+            },
+            child: Icon(
+              Icons.add,
+            ),
+          ),
+        ));
+  }
+}
